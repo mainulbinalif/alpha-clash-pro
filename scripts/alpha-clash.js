387ab61b8607e9fdd695b9ef5ba5c141dcc32398
mainulbinalif <mynulislam4432@gmail.com>
2024-02-14 19:18:11 +0600

    started a new js file name is utility

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+// function play() {
+//     // hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen')
+//     homeSection.classList.add('hidden');
+
+//     // show the playground
+//     const playGround = document.getElementById('playground-screen')
+//     playGround.classList.remove('hidden')
+// }
